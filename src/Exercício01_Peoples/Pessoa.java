a1b4defb13515316bf4e5313ae5dcc59e672deeb
Guilherme Sousa <guilherme.gs17@outloo.com>
2023-03-29 19:15:10 -0300

    initial commit

---
+++ src/Exercício01_Peoples/Pessoa.java
@@ -0,0 +1,29 @@
+package Exercício01_Peoples;
+
+public class Pessoa {
+    String nome, endereço, telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
